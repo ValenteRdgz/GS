2a636193d892e8974ba2566811700c525d12f534
ValenteRdgz <gato.berraco@gmail.com>
2026-08-31 13:26:35 -0700

    Update config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://ydzlgbjoxmkrmvayqjou.supabase.co';
+const SUPABASE_ANON_KEY = 'PEGA_AQUI_LA_CADENA_QUE_COPIASTE';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
